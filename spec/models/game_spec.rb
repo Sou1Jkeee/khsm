3870b6ab53b5399a27ef66acd2bd74d59f345981
Sou1Jkeee <1nfern0@list.ru>
2020-04-19 14:20:36 +0300

    homework solution 60-6(#previous_level and #current_game_question) fixed

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -77,7 +77,7 @@ RSpec.describe Game, type: :model do
       context 'during the game' do
         let(:correct_answer) { game_with_questions.current_game_question.correct_answer_key }
 
-        it 'right value for ech level' do
+        it 'right value for each level' do
           Question::QUESTION_LEVELS.max.times do
             game_with_questions.answer_current_question!(correct_answer)
             expect(game_with_questions.previous_level).to eq(game_with_questions.current_level - 1)

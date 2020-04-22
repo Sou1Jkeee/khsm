641a3bf91907b5ccbee38397d52f26d18791f1a3
Sou1Jkeee <1nfern0@list.ru>
2020-04-22 16:28:36 +0300

    homework solution 61-4(GameController anonymous user)

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -10,7 +10,49 @@ RSpec.describe GamesController, type: :controller do
   context 'Anon' do
     it 'kicked from #show' do
       get :show, id: game_with_questions.id
+      game = assigns(:game)
 
+      expect(game).to eq nil
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from #create' do
+      post :create
+      game = assigns(:game)
+
+      expect(game).to eq nil
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from #answer' do
+      put :answer, id: game_with_questions.id, letter: game_with_questions.current_game_question.correct_answer_key
+      game = assigns(:game)
+
+      expect(game).to eq nil
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from #help' do
+      put :help, id: game_with_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      expect(game).to eq nil
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from #take_money' do
+      put :take_money, id: game_with_questions.id
+      game = assigns(:game)
+
+      expect(game).to eq nil
       expect(response.status).not_to eq(200)
       expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to be
@@ -52,6 +94,39 @@ RSpec.describe GamesController, type: :controller do
       expect(game.current_level).to be > 0
       expect(response).to redirect_to(game_path(game))
       expect(flash.empty?).to be true
+    end
+
+    it 'alien game' do
+      alien_game = create(:game_with_questions)
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end
+
+    it '#take money' do
+      game_with_questions.update_attribute(:current_level, 10)
+      put :take_money, id: game_with_questions.id
+      game = assigns(:game)
+
+      expect(game.finished?).to be true
+      expect(game.prize).to eq(32_000)
+
+      user.reload
+
+      expect(user.balance).to eq(32_000)
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
+    end
+
+    it 'try to create second game' do
+      expect(game_with_questions.finished?).to be false
+
+      expect { post :create }.to change(Game, :count).by(0)
+
+      expect(response).to redirect_to(game_path(game_with_questions))
+      expect(flash[:alert]).to be
     end
   end
 end
